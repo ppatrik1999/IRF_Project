02f5944cc46830996113e5beeef5182cabe02f14
Patrik Pilinyi <patrik.pilinyi@stud.uni-corvinus.hu>
2020-12-07 16:47:11 +0100

    Product osztály létrehozása.

---
+++ Beadando_GMVW5Y/Entities/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beadando_GMVW5Y.Entities
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Manufacturer {get;set;}
+        public int PurchasedAmount { get; set; }
+        public int SaledAmount { get; set; }
+        public int AvailableAmount { get; set; }
+    }
+}
