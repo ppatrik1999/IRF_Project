fafa4ca54212aab51b723b9c4890faa978b8844a
Patrik Pilinyi <patrik.pilinyi@stud.uni-corvinus.hu>
2020-12-07 17:57:27 +0100

    Termék.csv file beolvasása és datagriedview-ba helyezése.

--- Beadando_GMVW5Y/Form1.cs
+++ Beadando_GMVW5Y/Form1.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Beadando_GMVW5Y.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +15,35 @@ namespace Beadando_GMVW5Y
 {
     public partial class Form1 : Form
     {
+        List<Product> Store = new List<Product>();
         public Form1()
         {
             InitializeComponent();
+            Store = GetStore("termék.csv");
+            dataGridView1.DataSource = Store;
         }
+        public List<Product> GetStore(string csvpath)
+        {
+            List<Product> store = new List<Product>();
+            using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var line = sr.ReadLine().Split(';');
+                    store.Add(new Product()
+                    {                       
+                        ID = int.Parse(line[0]),
+                        Name = line[1],
+                        Manufacturer = line[2],
+                        PurchasedAmount = int.Parse(line[3]),
+                        SaledAmount = int.Parse(line[4]),
+                        AvailableAmount = int.Parse(line[5])
+                    });
+                }
+            }
+            return store;
+        }
+        
+        
     }
 }
