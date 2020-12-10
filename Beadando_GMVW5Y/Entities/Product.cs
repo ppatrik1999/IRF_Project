330113b6d818114c9e3d2b79d286c08404cc9b03
Patrik Pilinyi <patrik.pilinyi@stud.uni-corvinus.hu>
2020-12-10 12:11:04 +0100

    Notavailable termékek listájának meghatározása.

--- Beadando_GMVW5Y/Entities/Product.cs
+++ Beadando_GMVW5Y/Entities/Product.cs
@@ -10,10 +10,11 @@ namespace Beadando_GMVW5Y.Entities
     public class Product
     {
         public int ID { get; set; }
-        public string Name { get; set; }
-        public string Manufacturer {get;set;}
-        public int PurchasedAmount { get; set; }
-        public int SaledAmount { get; set; }
-        public int AvailableAmount { get; set; }
+        public string Név { get; set; }
+        public string Gyártó {get;set;}
+        public int Beszerzett_mennyiség { get; set; }
+        public int Eladott_mennyiség { get; set; }
+        public int Elérhető_mennyiség { get; set; }
+        public int Egységár { get; set; }
     }
 }
--- Beadando_GMVW5Y/Form1.cs
+++ Beadando_GMVW5Y/Form1.cs
@@ -16,34 +16,60 @@ namespace Beadando_GMVW5Y
     public partial class Form1 : Form
     {
         List<Product> Store = new List<Product>();
+        List<Product> AvailableProducts = new List<Product>();
+        List<Product> NotAvailableProducts = new List<Product>();
         public Form1()
         {
             InitializeComponent();
             Store = GetStore("termék.csv");
             dataGridView1.DataSource = Store;
         }
+
         public List<Product> GetStore(string csvpath)
         {
             List<Product> store = new List<Product>();
+           
             using (StreamReader sr = new StreamReader(csvpath, Encoding.Default))
             {
                 while (!sr.EndOfStream)
                 {
                     var line = sr.ReadLine().Split(';');
                     store.Add(new Product()
-                    {                       
+                    {
                         ID = int.Parse(line[0]),
-                        Name = line[1],
-                        Manufacturer = line[2],
-                        PurchasedAmount = int.Parse(line[3]),
-                        SaledAmount = int.Parse(line[4]),
-                        AvailableAmount = int.Parse(line[5])
+                        Név = line[1],
+                        Gyártó = line[2],
+                        Beszerzett_mennyiség = int.Parse(line[3]),
+                        Eladott_mennyiség = int.Parse(line[4]),
+                        Elérhető_mennyiség = int.Parse(line[5]),
+                        Egységár = int.Parse(line[6])
                     });
                 }
             }
             return store;
         }
-        
-        
+
+        public void GetDelete()
+        {
+            AvailableProducts.Clear();
+            NotAvailableProducts.Clear();
+            
+            foreach (var s in Store)
+            {
+                if (s.Elérhető_mennyiség == 0 )
+                {
+                    NotAvailableProducts.Add(s);                  
+                }
+                else
+                {
+                    AvailableProducts.Add(s);
+                }
+            }
+            
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            GetDelete();
+        }
     }
 }
