b9177b510b58c3539f56656b1b0a1ed4c51a8f4e
Patrik Pilinyi <patrik.pilinyi@stud.uni-corvinus.hu>
2020-12-15 16:34:42 +0100

    Bevetel éss Producticture osztály Entities mappába szervezése

---
+++ Beadando_GMVW5Y/Entities/Bevetel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beadando_GMVW5Y.Entities
+{
+   public class Bevetel : System.Windows.Forms.Label
+    {
+        public string text { get; set; }
+        public Bevetel(string text)
+        {
+            Height = 30;
+            Width = 50;
+
+            this.Text = text;
+            TextAlign = ContentAlignment.MiddleCenter;
+        }
+    }
+}
---
+++ Beadando_GMVW5Y/Entities/ProductPicture.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Beadando_GMVW5Y.Entities
+{
+        public class ProductPicture : PictureBox
+        {
+            public ProductPicture(string kep, int sor, int oszlop)
+            {
+                this.Load(kep);
+                this.Height = this.Image.Height;
+                this.Width = this.Image.Width;
+                this.Top = this.Image.Height * sor;
+                this.Left = this.Image.Width * oszlop;
+            }
+
+
+        }
+    }
